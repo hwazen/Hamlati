4bb3b31f465fe60870abc24d6494fb6a80a3615c
hwazen <hwazen.7@hotmail.com>
2018-02-22 10:24:55 +0300

    Update LoginActivity.java

--- app/src/main/java/sa/Hamlati/project/LoginActivity.java
+++ app/src/main/java/sa/Hamlati/project/LoginActivity.java
@@ -5,7 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
-//just testing ver 2
+//i got delete this
 public class LoginActivity extends AppCompatActivity {
 
     @Override
