ba78b4a6a1a5c789ffff9be24964d6639d3bf915
hwazen <hwazen.7@hotmail.com>
2018-02-22 10:18:45 +0300

    Update LoginActivity.java
    add small commint

--- app/src/main/java/sa/Hamlati/project/LoginActivity.java
+++ app/src/main/java/sa/Hamlati/project/LoginActivity.java
@@ -5,7 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
-
+//just for test gitHub 
 public class LoginActivity extends AppCompatActivity {
 
     @Override
